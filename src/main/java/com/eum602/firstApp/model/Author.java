26621631df01bfb023dc7f02a03697e81cdc07e9
Erick <eum602@gmail.com>
2020-05-16 18:12:09 -0500

    adds JPA ids for the models

--- src/main/java/com/eum602/firstApp/model/Author.java
+++ src/main/java/com/eum602/firstApp/model/Author.java
@@ -1,11 +1,27 @@
 package com.eum602.firstApp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
-
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
     private Set<Book> books;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
--- src/main/java/com/eum602/firstApp/model/Book.java
+++ src/main/java/com/eum602/firstApp/model/Book.java
@@ -1,11 +1,26 @@
 package com.eum602.firstApp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
-
+@Entity
 public class Book {
     private String title;
     private String isbn;
     private Set<Author> authors;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Set<Author> getAuthors() {
         return authors;
