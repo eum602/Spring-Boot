b393455349e10f561c7b1beb2790c968c9cd759c
Erick <eum602@gmail.com>
2020-05-16 13:06:22 -0500

    adds models ==>POJOs(plain old java object) <== for Author and Books

---
+++ src/main/java/com/eum602/firstApp/model/Book.java
@@ -0,0 +1,33 @@
+package com.eum602.firstApp.model;
+
+import java.util.Set;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
