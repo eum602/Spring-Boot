c3cd31b79db78da92100720ec57d7742a50c23c2
Erick <eum602@gmail.com>
2020-05-16 19:47:24 -0500

    adds a publiser pojo, publisher repository and publisher to the bootstrap example

--- src/main/java/com/eum602/firstApp/bootstrap/BootstrapData.java
+++ src/main/java/com/eum602/firstApp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.eum602.firstApp.bootstrap;
 
 import com.eum602.firstApp.model.Author;
 import com.eum602.firstApp.model.Book;
+import com.eum602.firstApp.model.Publisher;
 import com.eum602.firstApp.repositories.AuthorRepository;
 import com.eum602.firstApp.repositories.BookRepository;
+import com.eum602.firstApp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private  final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {//dependency injection
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,PublisherRepository publisherRepository) {//dependency injection
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository=publisherRepository;
     }
 
     @Override
@@ -36,5 +40,11 @@ public class BootstrapData implements CommandLineRunner {
 
         System.out.println("Started in bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher publisher = new Publisher("Yessica","NN","NN","Avenue ...","123");
+        publisherRepository.save(publisher);
+        System.out.println("After adding a publisher to the h2 in memory database: ");
+        System.out.println("Number of publishers: " + publisherRepository.count());
+
     }
 }
---
+++ src/main/java/com/eum602/firstApp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.eum602.firstApp.repositories;
+
+import com.eum602.firstApp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+}
