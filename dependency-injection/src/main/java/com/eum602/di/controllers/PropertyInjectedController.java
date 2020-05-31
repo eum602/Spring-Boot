b5feec2d37f8ad6ad691e20f4744347d66572c64
Erick <eum602@gmail.com>
2020-05-31 11:51:22 -0500

    creates a controller and its testing file wit alt + insert ==> test (selecting before each)

---
+++ dependency-injection/src/main/java/com/eum602/di/controllers/PropertyInjectedController.java
@@ -0,0 +1,11 @@
+package com.eum602.di.controllers;
+
+import com.eum602.di.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ dependency-injection/src/test/java/com/eum602/di/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,18 @@
+package com.eum602.di.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getGreeting() {
+    }
+}
