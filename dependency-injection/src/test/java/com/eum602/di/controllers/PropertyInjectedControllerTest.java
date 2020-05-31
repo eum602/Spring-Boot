5b717beeb08b95f7de75962d535337e5ebc06e6f
Erick <eum602@gmail.com>
2020-05-31 12:01:29 -0500

    adds testing code for property injected controlller example

--- dependency-injection/src/test/java/com/eum602/di/controllers/PropertyInjectedControllerTest.java
+++ dependency-injection/src/test/java/com/eum602/di/controllers/PropertyInjectedControllerTest.java
@@ -1,18 +1,21 @@
 package com.eum602.di.controllers;
 
+import com.eum602.di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
-
+    PropertyInjectedController controller;
     @BeforeEach
     void setUp() {
-
+        controller = new PropertyInjectedController();
+        controller.greetingService = new GreetingServiceImpl();//assigning the Greeting service implementation to our greeting service PROPERTY
     }
 
     @Test
     void getGreeting() {
+        System.out.println(controller.getGreeting());
     }
 }
