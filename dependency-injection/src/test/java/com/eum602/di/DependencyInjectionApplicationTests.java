ae481ce05f69cf27b86866453d4b987d1bfbed11
Erick <eum602@gmail.com>
2020-05-30 23:17:26 -0500

    adds basic structure for dependency injection example project

---
+++ dependency-injection/src/test/java/com/eum602/di/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eum602.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
