db224432f9e98fbbe593b24221659e1b1e513588
Erick <eum602@gmail.com>
2020-05-31 11:38:02 -0500

    adds an example how to create a service interface and implementation

---
+++ dependency-injection/src/main/java/com/eum602/di/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.eum602.di.services;
+
+public interface GreetingService {
+    public String sayGreeting();
+}
---
+++ dependency-injection/src/main/java/com/eum602/di/services/GreetingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.eum602.di.services;
+
+public class GreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World!!";
+    }
+}
