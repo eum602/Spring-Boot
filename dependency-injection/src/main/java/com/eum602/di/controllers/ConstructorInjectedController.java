139eb61f8a30bdebc28b7655dba6c9cee3c02cc4
Erick <eum602@gmail.com>
2020-05-31 12:13:09 -0500

    adds testing code for the case when injecting by using a constructor inside the controller

---
+++ dependency-injection/src/main/java/com/eum602/di/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package com.eum602.di.controllers;
+
+import com.eum602.di.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
