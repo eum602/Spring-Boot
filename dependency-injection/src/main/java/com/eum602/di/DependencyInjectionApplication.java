122f12034607da4b4a231f40c287ad9eea23dfe9
Erick <eum602@gmail.com>
2020-05-31 12:38:43 -0500

    adds code to allow spring to make AUTOMATIC dependency injection by simple using @Controller and @Service(previously set). Autowired is not needed when using dependency injection through constructors in controllers

--- dependency-injection/src/main/java/com/eum602/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/eum602/di/DependencyInjectionApplication.java
@@ -1,5 +1,6 @@
 package com.eum602.di;
 
+import com.eum602.di.controllers.ConstructorInjectedController;
 import com.eum602.di.controllers.Controller1;
 import com.eum602.di.controllers.PropertyInjectedController;
 import com.eum602.di.controllers.SetterInjectedController;
@@ -23,7 +24,11 @@ public class DependencyInjectionApplication {
 
 		System.out.println("---------Setter Injected Controller");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());;
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("----------Constructor Injected Controller");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- dependency-injection/src/main/java/com/eum602/di/controllers/ConstructorInjectedController.java
+++ dependency-injection/src/main/java/com/eum602/di/controllers/ConstructorInjectedController.java
@@ -1,10 +1,12 @@
 package com.eum602.di.controllers;
 
 import com.eum602.di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
-
+    //here it is not necessary to use autowired because spring  understands here should be made automatic injection when using contructor on the controller
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
