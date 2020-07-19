fe6e497deed78d08efbd6f98224f97025d050aef
Erick <eum602@gmail.com>
2020-07-19 18:55:02 -0500

    moved spring web dependencies to pet-clinic-web module and added this to the main pom.xml

---
+++ pet-clinic/pet-clinic-web/src/test/java/com/eum602/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,12 @@
+package com.eum602.petclinic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetClinicApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
