241dec867a9a06156966cf8519386857757e2184
Erick <eum602@gmail.com>
2020-07-19 23:51:25 -0500

    adds interfaces for services so we can perform crud operations like find and save

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/eum602/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.eum602.petclinic.services;
+
+import com.eum602.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
